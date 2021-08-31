9cd775f763069efec5e14af6ffad35d82f37fef6
Alexander Trost <galexrt@googlemail.com>
2021-08-31 12:15:45 +0200

    update go mods
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- data/assets.go
+++ data/assets.go
@@ -1,10 +1,3 @@
-/*
-Copyright 2020 Alexander Trost <galexrt@googlemail.com>
-
-This work is licensed under the terms of the MIT license.
-For a copy, see <https://opensource.org/licenses/MIT>.
-*/
-
 package data
 
 import (
@@ -13,29 +6,29 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Assetse48cd5158def07113582a8696d146a718b0c2911 = "<!DOCTYPE html>\n<html>\n    <head>\n        <title>goytdler - Simple webinterface to use youtube-dl</title>\n    </head>\n    <body>\n        <h1>goytdler</h1>\n        <p>Simple webinterface to use youtube-dl.</p>\n        <hr>\n        <form method=\"POST\" action=\"{{ .RoutesBasePath }}download\">\n            <input type=\"text\" name=\"url\" placeholder=\"Youtube-DL compatible Link\" />\n            <input type=\"submit\" value=\"Submit\" />\n        </form>\n    </body>\n</html>\n"
 var _Assets630f88879f98b6e0f05588023557b3717e54a9ae = "<!DOCTYPE html>\n<html>\n    <head>\n        <title>goytdler - Simple webinterface to use youtube-dl</title>\n    </head>\n    <body>\n        <h1>goytdler</h1>\n        <p>Simple webinterface to use youtube-dl.</p>\n        <hr>\n        <h2>Error</h2>\n        <p>{{ . }}</p>\n    </body>\n</html>\n"
+var _Assetse48cd5158def07113582a8696d146a718b0c2911 = "<!DOCTYPE html>\n<html>\n    <head>\n        <title>goytdler - Simple webinterface to use youtube-dl</title>\n    </head>\n    <body>\n        <h1>goytdler</h1>\n        <p>Simple webinterface to use youtube-dl.</p>\n        <hr>\n        <form method=\"POST\" action=\"{{ .RoutesBasePath }}download\">\n            <input type=\"text\" name=\"url\" placeholder=\"Youtube-DL compatible Link\" />\n            <input type=\"submit\" value=\"Submit\" />\n        </form>\n    </body>\n</html>\n"
 
 // Assets returns go-assets FileSystem
 var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"templates"}, "/templates": []string{"index.tmpl", "error.tmpl"}}, map[string]*assets.File{
-	"/templates": &assets.File{
-		Path:     "/templates",
-		FileMode: 0x800001ed,
-		Mtime:    time.Unix(1524760173, 1524760173068371739),
-		Data:     nil,
-	}, "/templates/index.tmpl": &assets.File{
-		Path:     "/templates/index.tmpl",
-		FileMode: 0x1a4,
-		Mtime:    time.Unix(1524825723, 1524825723310010286),
-		Data:     []byte(_Assetse48cd5158def07113582a8696d146a718b0c2911),
-	}, "/templates/error.tmpl": &assets.File{
+	"/templates/error.tmpl": &assets.File{
 		Path:     "/templates/error.tmpl",
 		FileMode: 0x1a4,
-		Mtime:    time.Unix(1524762038, 1524762038861083087),
+		Mtime:    time.Unix(1598805499, 1598805499899794928),
 		Data:     []byte(_Assets630f88879f98b6e0f05588023557b3717e54a9ae),
 	}, "/": &assets.File{
 		Path:     "/",
 		FileMode: 0x800001ed,
-		Mtime:    time.Unix(1524825340, 1524825340852696592),
+		Mtime:    time.Unix(1630404847, 1630404847399251590),
 		Data:     nil,
+	}, "/templates": &assets.File{
+		Path:     "/templates",
+		FileMode: 0x800001ed,
+		Mtime:    time.Unix(1598805499, 1598805499899794928),
+		Data:     nil,
+	}, "/templates/index.tmpl": &assets.File{
+		Path:     "/templates/index.tmpl",
+		FileMode: 0x1a4,
+		Mtime:    time.Unix(1598805499, 1598805499899794928),
+		Data:     []byte(_Assetse48cd5158def07113582a8696d146a718b0c2911),
 	}}, "")
--- go.mod
+++ go.mod
@@ -1,21 +1,30 @@
-module github.com/galexrt/goytldler
+module github.com/galexrt/goytdler
 
 go 1.14
 
 require (
+	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f
-	github.com/galexrt/goytdler v0.0.5
-	github.com/gin-gonic/gin v1.6.3
-	github.com/go-playground/validator/v10 v10.3.0 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/prometheus/client_golang v1.6.0 // indirect
-	github.com/prometheus/common v0.10.0
-	github.com/prometheus/procfs v0.1.0 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	github.com/jessevdk/go-assets-builder v0.0.0-20130903091706-b8483521738f // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/prometheus/common v0.30.0
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/prometheus/promu v0.12.0 // indirect
+	github.com/ugorji/go v1.2.6 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
